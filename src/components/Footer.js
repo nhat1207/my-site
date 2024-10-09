4111f3690839822a65536b6e8dbaf85e90beee3b
Tuan Nhat <nhatcco@gmail.com>
2024-10-09 15:02:51 +0700

    p - footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer  text-center">
-      <p>&copy; 2024 My Website. All rights reserved.</p>
+      <p>&copy; Nhat 2024 My Website. All rights reserved.</p>
     </footer>
   );
 };
